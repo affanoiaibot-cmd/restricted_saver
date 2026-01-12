19c7d6c7563efa0e870a0aaee138b421113375a8
JAFFER NOVA <jaffernova21@gmail.com>
2026-01-13 01:21:12 +0530

    Refactor config settings for ADMINS and add PORT
    Updated ADMINS to be a list and added PORT for health check.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 import os
 
 # Login feature, if you want then True , if you don't want then False
-LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True)) # True or False
+LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True)) 
 
 if LOGIN_SYSTEM == False:
     # if login system is False then fill your tg account session below 
@@ -22,21 +22,21 @@ API_ID = int(os.environ.get("API_ID", "35554205"))
 # Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "7b56a1a17366fb67ba913ff0cbac6e67")
 
-# Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "8281644724"))
+# --- IMPORTANT CHANGE: ADMINS must be a list ---
+ADMINS = [8281644724] 
 
-# Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
-# And Make Your Bot Admin In this channel with full rights.
-# if you don't want to upload in channel then leave it blank don't fill anything.
+# Your Channel Id In Which Bot Uploads
 CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 
 # Your Mongodb Database Url
-# Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://jaffernova21_db_user:kdcxktjJo6dEXEWm@cluster0.rdci8wh.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://jaffernova21_db_user:kdcxktjJo6dEXEWm@cluster0.rdci8wh.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
-# Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
-WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
+# Increase time to avoid floodwait
+WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) 
 
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+# Error Message setting
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+
+# --- NEW: Port for Koyeb Health Check ---
+PORT = int(os.environ.get("PORT", "8080"))
