56396f5f2adea225a35018effa8ec0446c82ceb6
jaffernova21-dot <jaffernova21@gmail.com>
2026-01-12 20:46:58 +0530

    Change hello_world response and run configuration
    Updated the hello_world function response and modified the app run configuration for Koyeb deployment.

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+    # Ye text Koyeb ke logs mein dikhega
+    return 'VJ Bots - Bot is Running Live and Healthy!'
 
-
-if __name__ == "__main__":
-    app.run()
+def run_web():
+    # Koyeb ke liye 0.0.0.0 aur port 8080 zaroori hai
+    app.run(host='0.0.0.0', port=8080)
