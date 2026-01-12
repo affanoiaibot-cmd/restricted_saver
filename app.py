329a301fcce891dab304e37a976fbd116d8a27db
JAFFER NOVA <jaffernova21@gmail.com>
2026-01-13 01:23:49 +0530

    Configure logging to suppress health check logs
    Added logging configuration to suppress Werkzeug logs.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import logging
+
+# Flask ke faltu pings (health checks) logs mein na dikhein, uske liye:
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Ye text Koyeb ke logs mein dikhega
+    # Jab Koyeb is link par aayega, use ye message milega
     return 'VJ Bots - Bot is Running Live and Healthy!'
 
 def run_web():
-    # Koyeb ke liye 0.0.0.0 aur port 8080 zaroori hai
+    # '0.0.0.0' isliye zaroori hai taaki Koyeb bot ko internet se connect kar sake
+    # Port 8080 Koyeb ki default setting hoti hai
     app.run(host='0.0.0.0', port=8080)
