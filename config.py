83d744b1c022d510e812c367e6b9cb4c3982d581
affanoiaibot-cmd <affanoiaibot@gmail.com>
2026-04-08 00:32:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8613803836:AAGtn4eRuT9YO8QB4XeiW-TABhjk8emSkT8")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "35554205"))
