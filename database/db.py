c27a1904946ec5dd00dc4149a0a87e13aa639b6a
affanoiaibot-cmd <affanoiaibot@gmail.com>
2026-04-08 00:21:43 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_B
-# Subscribe YouTube Channel F
-# Ask Doubt on telegram 
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
@@ -60,9 +60,5 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return user.get('api_hash')
 
-db = Database(DB_URI, "TechVJDemoBot")
-
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-                                       
+# Yahan par maine DB_NAME use kiya hai config.py se
+db = Database(DB_URI, DB_NAME)
